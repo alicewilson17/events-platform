7cfe87e90773f229d859e5ff99eb9fb9690a0557
Alice Wilson <alice.g.wilson@hotmail.co.uk>
2024-10-28 12:14:37 +0000

    add order by date asc to all evens queries

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,7 +16,7 @@ exports.createUser = async (first_name, last_name, email, password, role) => {
 
 //get signups by user
 exports.fetchSignupsByUser = async(user_id) => {
-const res = await db.query('SELECT events.event_id, events.title, events.description, events.date, events.start_time, events.end_time, events.location, events.img FROM signups JOIN events ON signups.event_id = events.event_id WHERE signups.user_id = $1;', [user_id])
+const res = await db.query('SELECT events.event_id, events.title, events.description, events.date, events.start_time, events.end_time, events.location, events.img FROM signups JOIN events ON signups.event_id = events.event_id WHERE signups.user_id = $1 ORDER BY events.date asc;', [user_id])
 return res.rows
 }
 
@@ -26,6 +26,6 @@ exports.fetchAdminEventsWithSignups = async(user_id) => {
                 SELECT events.*, COUNT(signups.id) AS signup_count
                 FROM events LEFT JOIN signups ON events.event_id = signups.event_id
                 WHERE events.created_by = $1
-                GROUP BY events.event_id;`, [user_id])
+                GROUP BY events.event_id ORDER BY events.date asc;`, [user_id])
         return res.rows
 }
