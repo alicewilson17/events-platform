1d11d9ca426b0eb7d1e33d53821a11cb38334603
Alice Wilson <alice.g.wilson@hotmail.co.uk>
2024-11-07 12:44:04 +0000

    fixed delete event function by ensuring signups for deleted event are also deleted.

--- backend/db/seeds/seeds.js
+++ backend/db/seeds/seeds.js
@@ -1,16 +1,16 @@
-const db = require('../db');
-const bcrypt = require('bcrypt')
-const format = require('pg-format')
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const format = require("pg-format");
 
-const seedDatabase = async ({eventsData, usersData}) => {
-    try {
-        //drop tables if they exist
-        await db.query ('DROP TABLE IF EXISTS signups')
-        await db.query('DROP TABLE IF EXISTS events;');
-        await db.query('DROP TABLE IF EXISTS users;');
+const seedDatabase = async ({ eventsData, usersData }) => {
+  try {
+    //drop tables if they exist
+    await db.query("DROP TABLE IF EXISTS signups");
+    await db.query("DROP TABLE IF EXISTS events;");
+    await db.query("DROP TABLE IF EXISTS users;");
 
-        //create tables
-        await db.query(`CREATE TABLE users (
+    //create tables
+    await db.query(`CREATE TABLE users (
                 user_id SERIAL PRIMARY KEY,
                 first_name VARCHAR(255) NOT NULL,
                 last_name VARCHAR(255) NOT NULL,
@@ -18,8 +18,8 @@ const seedDatabase = async ({eventsData, usersData}) => {
                 password VARCHAR(255) NOT NULL,
                 role VARCHAR(50) NOT NULL
             );
-            `)
-  await db.query(`
+            `);
+    await db.query(`
     CREATE TABLE events (
       event_id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
@@ -33,32 +33,70 @@ const seedDatabase = async ({eventsData, usersData}) => {
         created_by INTEGER REFERENCES users(user_id),
         img VARCHAR(255)
     );
-    `)
+    `);
 
     await db.query(`
-      CREATE TABLE signups (id SERIAL PRIMARY KEY, event_id INTEGER REFERENCES events(event_id), user_id INTEGER REFERENCES users(user_id), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`)
+      CREATE TABLE signups (
+        id SERIAL PRIMARY KEY, 
+        event_id INTEGER REFERENCES events(event_id) ON DELETE CASCADE, 
+        user_id INTEGER REFERENCES users(user_id), 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
 
-    const hashedUsers = await Promise.all(usersData.map(async user => {
+    const hashedUsers = await Promise.all(
+      usersData.map(async (user) => {
         const hashedPassword = await bcrypt.hash(user.password, 10);
-        return [user.first_name, user.last_name, user.email, hashedPassword, user.role];
-    }));
+        return [
+          user.first_name,
+          user.last_name,
+          user.email,
+          hashedPassword,
+          user.role,
+        ];
+      })
+    );
 
-// Seed users
-await db.query(format(
-    'INSERT INTO users (first_name, last_name, email, password, role) VALUES %L;',
-  hashedUsers
-  ));
+    // Seed users
+    await db.query(
+      format(
+        "INSERT INTO users (first_name, last_name, email, password, role) VALUES %L;",
+        hashedUsers
+      )
+    );
 
-  // Seed events
-  await db.query(format(
-    'INSERT INTO events (title, description, date, start_time, end_time, location, price, is_paid, created_by, img) VALUES %L;',
-    eventsData.map(({ title, description, date, start_time, end_time, location, price, is_paid, created_by, img }) => 
-      [title, description, date, start_time, end_time, location, price, is_paid, created_by, img])
-  ));
-
-    } catch (error) {
-        console.error('Error seeding database:', error);
-    }
+    // Seed events
+    await db.query(
+      format(
+        "INSERT INTO events (title, description, date, start_time, end_time, location, price, is_paid, created_by, img) VALUES %L;",
+        eventsData.map(
+          ({
+            title,
+            description,
+            date,
+            start_time,
+            end_time,
+            location,
+            price,
+            is_paid,
+            created_by,
+            img,
+          }) => [
+            title,
+            description,
+            date,
+            start_time,
+            end_time,
+            location,
+            price,
+            is_paid,
+            created_by,
+            img,
+          ]
+        )
+      )
+    );
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
 };
 
 module.exports = seedDatabase;
